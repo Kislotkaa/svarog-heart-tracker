b1b5f476961af1d5ace11af65f73945d68eab3a8
Maksihill <Maksihill@yandex.ru>
2023-03-26 14:11:38 +0500

    2.1.17+21

--- lib/app/cache/start_app_cache.dart
+++ lib/app/cache/start_app_cache.dart
@@ -1,5 +1,6 @@
 import '../models/start_app_model.dart';
 import 'abstarct_data_source.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class StartAppCache extends AbstractDataSource {
   @override
@@ -7,20 +8,28 @@ class StartAppCache extends AbstractDataSource {
 
   @override
   Future<void> setData(param) async {
-    return await box.write(key, param);
+    await sharedPreferences?.setString(key, param);
+    // return await box.write(key, param);
   }
 
   @override
   StartAppModel? getData() {
-    var data = box.read(key);
+    var data = sharedPreferences?.getString(key);
     if (data != null) {
       return StartAppModel.fromJson(data);
     }
+
+    // var data = box.read(key);
+    // if (data != null) {
+    //   return StartAppModel.fromJson(data);
+    // }
+
     return null;
   }
 
   @override
   void clearData() {
-    box.remove(key);
+    sharedPreferences?.remove(key);
+    // box.remove(key);
   }
 }
