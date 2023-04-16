4f3c0fca247052daad7e51ed315e51787337384d
Maksihill <Maksihill@yandex.ru>
2023-04-16 08:55:05 +0300

    fix sentry

--- lib/app/helper/error_handler.dart
+++ lib/app/helper/error_handler.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:svarog_heart_tracker/app/controllers/base_snackbar_controller.dart';
@@ -8,9 +9,15 @@ class ErrorHandler {
   static getMessage(dynamic e, StackTrace s) async {
     if (e is FormatException) {
       showSnackbar(e.message, 'Ошибка');
+    } else if (e is PlatformException) {
+      if (e.code.contains('bluetooth')) {
+        Get.printError(info: e.toString());
+        Get.printError(info: s.toString());
+      }
+    } else {
+      Get.printError(info: e.toString());
+      Get.printError(info: s.toString());
+      Sentry.captureException(e, stackTrace: s);
     }
-    Get.printError(info: e.toString());
-    Get.printError(info: s.toString());
-    Sentry.captureException(e, stackTrace: s);
   }
 }
