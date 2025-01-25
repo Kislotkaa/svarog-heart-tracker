ed300f803e7d618e19b40b8aa5238740b4096d74
Maxim Feduishkin <maksihill@yandex.ru>
2025-01-26 00:01:40 +0500

    2.2.41+39

--- babundle.sh
+++ babundle.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-flutter build appbundle --release lib/main.dart
+flutter build appbundle --release lib/main.dart --dart-define=APP_PASSWORD=wziAeSE4Gqxh --dart-define=APP_POLIT_URL=https://doc-hosting.flycricket.io/heart-rate-privacy-policy/7798b6f0-c5bd-4751-b7b4-47d1dfdfa389/privacy
--- bapk.sh
+++ bapk.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-flutter build apk lib/main.dart
+flutter build apk lib/main.dart --dart-define=APP_PASSWORD=wziAeSE4Gqxh --dart-define=APP_POLIT_URL=https://doc-hosting.flycricket.io/heart-rate-privacy-policy/7798b6f0-c5bd-4751-b7b4-47d1dfdfa389/privacy
